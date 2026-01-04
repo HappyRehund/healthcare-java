c9ad34156aee3eec4bbb7757476425ed532f05de
HappyRehund <rayhanardian045@gmail.com>
2026-01-04 18:20:33 +0700

    update allowed origins in CORS configuration for development environment

--- src/main/java/com/rehund/healthcare/config/ApiSecurityConfig.java
+++ src/main/java/com/rehund/healthcare/config/ApiSecurityConfig.java
@@ -56,7 +56,7 @@ public class ApiSecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:8080", "http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("http://localhost:8080", "http://localhost:5173"));
         configuration.addAllowedMethod("*");
         configuration.addAllowedHeader("*");
         configuration.setAllowCredentials(true);
